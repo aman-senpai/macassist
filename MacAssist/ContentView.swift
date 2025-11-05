d8c9771e93a9042ca2f9be55dc61f44f37896a85
Aman Raj <jemsocllin1@gmail.com>
2025-11-05 13:04:00 +0530

    Initial Commit

---
+++ MacAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacAssist
+//
+//  Created by Aman Raj on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacAssist/MacAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAssistApp.swift
+//  MacAssist
+//
+//  Created by Aman Raj on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MacAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
